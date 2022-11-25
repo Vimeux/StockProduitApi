ba3a882fd0b2b015acbd941ba73fed4e34708969
Benjamin Vimeux <vimeux.benji@gmail.com>
2022-11-25 15:54:08 +0100

    remove old files

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-# from flask import Flask
-# from src.database.database import init_app
-#
-# # import the controllers
-#
-# app = Flask(__name__)
-#
-# # setup config
-# app.config.from_object('config.DevelopmentConfig')
-#
-# # initialize the database
-# init_app(app)
-#
-#
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
-
 from src import init_app
 
 app = init_app()
